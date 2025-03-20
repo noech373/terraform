a796a31cc7374b96268ac1a724c92f5d4d44472f
Noé Chabanon <noe.chabanon@gmail.com>
2025-03-20 16:21:12 +0100

    jjj

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "aws_lambda_function" "my_lambda" {
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "ParisTimeHousseinNoeAPI"
   protocol_type = "HTTP"
-}
+} 
 
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.http_api.id
