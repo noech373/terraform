0ab989d3d24f985c8bf1830a00d401ed19ae8ed1
Noé Chabanon <noe.chabanon@gmail.com>
2025-03-20 14:32:35 +0100

    first commit

---
+++ lambda/index.js
@@ -0,0 +1,13 @@
+exports.handler = async () => {
+    const timeZone = "Europe/Paris";
+    const currentTime = new Date().toLocaleString("fr-FR", { timeZone });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Heure actuelle à Paris et auteur",
+            time: currentTime,
+            author: "ChatGPT",
+        }),
+    };
+};
---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "ParisTimeLambda"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "nodejs18.x"
+  handler       = "index.handler"
+
+  filename = "lambda.zip"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      TIMEZONE = "Europe/Paris"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "ParisTimeAPI"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.my_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /time"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+output "api_url" {
+  value = "${aws_apigatewayv2_api.http_api.api_endpoint}/time"
+}
