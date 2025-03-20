880e8eb26ee97da063e804a7a9b19595a72a2f7e
Noé Chabanon <noe.chabanon@gmail.com>
2025-03-20 15:04:31 +0100

    change iamrole

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,19 +2,27 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_iam_role" "lambda_role" {
+resource "aws_iam_role" "lambda_exec" {
   name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
+}
+
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "lambda_basic_execution"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "my_lambda" {
