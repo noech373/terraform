27cf7d582bc30fe2a603bc158f7803301bfd2026
Noé Chabanon <noe.chabanon@gmail.com>
2025-03-20 15:16:51 +0100

    change role

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "aws_iam_policy_attachment" "lambda_basic_execution" {
   name       = "lambda_basic_execution"
   roles      = [aws_iam_role.lambda_exec.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-} 
+}  
 
 resource "aws_lambda_function" "my_lambda" {
   filename         = "../lambda.zip"  
